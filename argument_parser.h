f045b3fffed9bee8931c1ce6e49855edfdf17618
Aria Diniz <aria.diniz.dev@gmail.com>
2024-01-20 23:04:58 -0300

    implementing parse_args function

--- argument_parser.h
+++ argument_parser.h
@@ -2,5 +2,6 @@
 #define ARIA_C_ARGUMENT_PARSER_H
 
 #include "lib/simple_data_structures/datastructures.h"
+#include "src/parseargs.h"
 
 #endif // ARIA_C_ARGUMENT_PARSER_H
--- src/parseargs.c
+++ src/parseargs.c
@@ -1,11 +1,25 @@
 #include "parseargs.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 HashTable *parse_args(int argc, char *argv[]) {
   HashTable *table = createhash();
   if (argc == 1) {
     return table;
   }
+  char *first_char;
+  for (int i=1; i<argc; i++) {
+    first_char = argv[i];
+    printf("first char: %s\n", first_char);
+    if (i < argc - 1 && first_char != '-') {
+      printf("adding to hash\n");
+      addtohash(table, argv[i], argv[i + 1]);
+      i++;
+    } else {
+      addtohash(table, argv[i], argv[i]);
+    }
+  }
+  return table;
 }
 
 void call_help(HashTable *parsed_args) {}
